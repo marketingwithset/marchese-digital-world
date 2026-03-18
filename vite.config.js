04fda650b7b63276eb32c383fce21dc18f6714fb
marketingwithset <marketingwithset@gmail.com>
2026-03-18 12:08:27 -0400

    fix: use esbuild minifier instead of terser

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/',
-  build: { outDir: 'dist', assetsDir: 'assets', sourcemap: false, minify: 'terser' },
+  build: { outDir: 'dist', assetsDir: 'assets', sourcemap: false },
   server: { port: 3000 }
 });
